8fde8949875fe0ea36e76fae6b21e9dd89c40f6c
XaloEpshi <gonzalo.mellao.m@gmail.com>
2024-11-20 18:56:24 -0300

    cambios en index

--- index.js
+++ index.js
@@ -1,21 +1,65 @@
-const express = require("express");
-const bodyParse = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const { WebSocketServer } = require('ws');
+const pool = require('./config/db'); // Configuración de la base de datos
+const agendaRoutes = require('./routes/agendaRoutes');
+const nacionalRoutes = require('./routes/nacionalRoutes');
+const tareasRoutes = require('./routes/tareasRoutes');
+const inventarioRoutes = require('./routes/inventarioRoutes');
+const exportacionesRoutes = require('./routes/exportacionesRoutes');
+const calendarioRoutes = require('./routes/calendarioRoutes');
+const activitiesRoutes = require('./routes/activitiesRoutes');
+
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const port = process.env.PORT || 3001; // Usar variable de entorno o puerto por defecto
 
-app.use(bodyParse.urlencoded({ extended: true }));
-app.use(bodyParse.json());
+app.use(express.json()); 
+app.use(cors());
 
-app.get("/", (req, res) => {
-    res.status(200).send({ msg: "Hola TinCode!" });
+// Probar la conexión a la base de datos
+pool.getConnection()
+  .then((conn) => {
+    console.log("Conexión exitosa a la base de datos");
+    conn.release(); // Libera la conexión para que pueda ser usada por otros
+  })
+  .catch((err) => {
+    console.error("Error al conectar a la base de datos:", err.message);
+    process.exit(1); // Termina el proceso si no puede conectarse a la base de datos
+  });
+
+// Rutas
+app.use('/api/agenda', agendaRoutes); 
+app.use('/api', nacionalRoutes); 
+app.use('/api', tareasRoutes); 
+app.use('/api', inventarioRoutes); 
+app.use('/api/exportaciones', exportacionesRoutes);
+app.use('/api/calendario', calendarioRoutes); 
+app.use('/api/activities', activitiesRoutes);
+
+// Ruta de prueba para verificar que el servidor está funcionando
+app.get('/test', (req, res) => {
+  res.send('El servidor está funcionando correctamente!');
 });
 
-app.post("/welcome", (req, res) => {
-    const { username } = req.body;
-    res.status(200).send({ msg: `Hola, ${username}` });
+// Configuración del servidor WebSocket
+const server = app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor corriendo en el puerto ${port}`);
 });
 
-app.listen(PORT, () => {
-    console.log(`Tu server esta listo en el puerto ${PORT}`);
+const wss = new WebSocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Nuevo cliente conectado');
+
+  ws.on('message', (message) => {
+    console.log('Mensaje recibido del cliente:', message);
+    ws.send('Mensaje recibido por el servidor');
+  });
+
+  ws.on('close', () => {
+    console.log('Cliente desconectado');
+  });
 });
+
+module.exports = app;
