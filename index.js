ac4092d317f896f0136543527073d3ed5e5a48fd
XaloEpshi <gonzalo.mellao.m@gmail.com>
2024-11-20 23:02:08 -0300

    change port

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const activitiesRoutes = require('./routes/activitiesRoutes');
 
 
 const app = express();
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Servidor corriendo en el puerto ${port}`);
 });
