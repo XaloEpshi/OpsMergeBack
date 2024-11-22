b1df1dc16a937e626721e5626a0fdc0d3ce3bdda
XaloEpshi <gonzalo.mellao.m@gmail.com>
2024-11-22 18:47:51 -0300

    cambios activities 3

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -3,7 +3,7 @@ const pool = require('../config/db');
 // Obtener todos los registros de actividades
 exports.getAllActivities = async (req, res) => {
     try {
-        const [agendaDiaria] = await pool.query('SELECT "Agenda Diaria" AS origen, fecha AS fecha_hora, detalles AS actividad, "" AS descripcion, "" AS user_id FROM agenda_diaria');
+       
         const [despachoNacional] = await pool.query('SELECT "Despacho Nacional" AS origen, NOW() AS fecha_hora, CONCAT(cantidad, " unidades despachadas") AS actividad, CONCAT("Chofer: ", nombreChofer, ", Patente Camión: ", patenteCamion) AS descripcion, responsable, "" AS user_id FROM despacho_nacional');
         const [tareas] = await pool.query('SELECT "Tareas" AS origen, fecha_inicio AS fecha_hora, nombre_tarea AS actividad, descripcion AS descripcion, responsable AS user_id FROM tareas');
         const [inventarioBodegas] = await pool.query('SELECT "Inventario Bodegas" AS origen, fecha_inventario AS fecha_hora, nombre_bodega AS actividad, detalle_inventario AS descripcion, "" AS user_id FROM InventarioBodegas');
