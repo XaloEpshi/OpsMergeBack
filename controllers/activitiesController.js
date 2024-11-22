c72b9ce7e7da3095229a94c5feedf4b9cdb32426
XaloEpshi <gonzalo.mellao.m@gmail.com>
2024-11-22 18:54:50 -0300

    CAMBIO 5 ACTIVITIES

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -1,22 +1,18 @@
 const pool = require('../config/db');
 
-// Obtener todos los registros de actividades
+// Obtener todos los registros de actividades, excluyendo "Agenda Diaria" y "Calendario"
 exports.getAllActivities = async (req, res) => {
     try {
-       
-        const [despachoNacional] = await pool.query('SELECT "Despacho Nacional" AS origen, NOW() AS fecha_hora, CONCAT(cantidad, " unidades despachadas") AS actividad, CONCAT("Chofer: ", nombreChofer, ", Patente Camión: ", patenteCamion) AS descripcion, responsable, "" AS user_id FROM despacho_nacional');
+        const [despachoNacional] = await pool.query('SELECT "Despacho Nacional" AS origen, NOW() AS fecha_hora, CONCAT(cantidad, " unidades despachadas") AS actividad, CONCAT("Chofer: ", nombreChofer, ", Patente Camión: ", patenteCamion) AS descripcion, responsable AS user_id FROM despacho_nacional');
         const [tareas] = await pool.query('SELECT "Tareas" AS origen, fecha_inicio AS fecha_hora, nombre_tarea AS actividad, descripcion AS descripcion, responsable AS user_id FROM tareas');
-        const [inventarioBodegas] = await pool.query('SELECT "Inventario Bodegas" AS origen, fecha_inventario AS fecha_hora, nombre_bodega AS actividad, detalle_inventario AS descripcion, "" AS user_id FROM InventarioBodegas');
-        const [exportaciones] = await pool.query('SELECT "Exportaciones" AS origen, fechaCarga AS fecha_hora, CONCAT(material, " exportado a ", mercado) AS actividad, observacion AS descripcion, "" AS user_id FROM exportaciones');
-        const [calendario] = await pool.query('SELECT "Calendario" AS origen, start AS fecha_hora, title AS actividad, "" AS descripcion, userId AS user_id FROM Calendario');
-        
+        const [inventarioBodegas] = await pool.query('SELECT "Inventario Bodegas" AS origen, fecha_inventario AS fecha_hora, nombre_bodega AS actividad, detalle_inventario AS descripcion, responsable AS user_id FROM InventarioBodegas');
+        const [exportaciones] = await pool.query('SELECT "Exportaciones" AS origen, fechaCarga AS fecha_hora, CONCAT(material, " exportado a ", mercado) AS actividad, observacion AS descripcion, responsable AS user_id FROM exportaciones');
+
         const activities = [
-            ...agendaDiaria,
             ...despachoNacional,
             ...tareas,
             ...inventarioBodegas,
-            ...exportaciones,
-            ...calendario,
+            ...exportaciones
         ];
 
         res.json(activities);
