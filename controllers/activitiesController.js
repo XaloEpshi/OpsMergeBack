6753d03cf7b07cfd2a1512f76b1c369b049a26c2
XaloEpshi <gonzalo.mellao.m@gmail.com>
2024-11-24 21:27:33 -0300

    Agregue turno en activities exportaciones

--- controllers/activitiesController.js
+++ controllers/activitiesController.js
@@ -6,7 +6,8 @@ exports.getAllActivities = async (req, res) => {
         const [despachoNacional] = await pool.query('SELECT "Despacho Nacional" AS origen, NOW() AS fecha_hora, CONCAT(cantidad, " unidades despachadas") AS actividad, CONCAT("Chofer: ", nombreChofer, ", Patente Camión: ", patenteCamion) AS descripcion, responsable AS user_id FROM despacho_nacional');
         const [tareas] = await pool.query('SELECT "Tareas" AS origen, fecha_inicio AS fecha_hora, nombre_tarea AS actividad, descripcion AS descripcion, responsable AS user_id FROM tareas');
         const [inventarioBodegas] = await pool.query('SELECT "Inventario Bodegas" AS origen, fecha_inventario AS fecha_hora, nombre_bodega AS actividad, detalle_inventario AS descripcion, responsable AS user_id FROM InventarioBodegas');
-        const [exportaciones] = await pool.query('SELECT "Exportaciones" AS origen, fechaCarga AS fecha_hora, CONCAT(material, " exportado a ", mercado) AS actividad, observacion AS descripcion, "" AS user_id FROM exportaciones');
+        const [exportaciones] = await pool.query('SELECT "Exportaciones" AS origen, fechaCarga AS fecha_hora, CONCAT(material, " exportado a ", mercado) AS actividad, observacion AS descripcion, turno, "" AS user_id FROM exportaciones');
+
 
         const activities = [
             ...despachoNacional,
